bb11ad65f18c2b6c6be59d9c8a2dce233dc14ac8
Narayan Pokhrel <56493136+npokhrel163@users.noreply.github.com>
2024-11-05 17:02:59 -0500

    Add files via upload

---
+++ Weyl_formula.py
@@ -0,0 +1,40 @@
+#calculates the weyl formula for number of size of total CI expansion
+
+'''
+@author = npokh
+'''
+
+#arguments are
+#N_elect = number of electrons
+#n_orbital = number of orbitals
+#N_spin = spin(s) 0 for singlet 1 for triplet
+
+
+from math import factorial
+def compute_binomial(x,y):
+    binomial = (factorial(x))/((factorial(y)*(factorial(x-y))))
+    return int(binomial)
+
+def compute_weyl(N_elect, n_orbital, N_spin):
+    comb1 = compute_binomial(n_orbital+1,N_elect/2-N_spin)
+    comb2 = compute_binomial(n_orbital+1, N_elect/2+N_spin+1)
+    N_Cas = (2*N_spin+1)/(n_orbital+1) * comb1 * comb2
+    return N_Cas
+
+
+print(compute_weyl(2,2,0))
+print(compute_weyl(2,2,1))
+
+print(compute_weyl(4,3,0))
+print(compute_weyl(4,3,1))
+
+print(compute_weyl(6,4,0))
+print(compute_weyl(6,4,1))
+#print(compute_binomial(4,1))
+#print(compute_binomial(4,2))
+#print(compute_binomial(4,3))
+#print(compute_binomial(4,4))
+
+
+
+
