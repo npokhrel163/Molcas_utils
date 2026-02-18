141868a130888db32ec04e5b7dbe501807f2cccf
Narayan Pokhrel <56493136+npokhrel163@users.noreply.github.com>
2026-02-17 21:47:55 -0500

    Update Weyl_formula.py

--- Weyl_formula.py
+++ Weyl_formula.py
@@ -16,8 +16,8 @@ def compute_binomial(x,y):
     return int(binomial)
 
 def compute_weyl(N_elect, n_orbital, N_spin):
-    comb1 = compute_binomial(n_orbital+1,N_elect/2-N_spin)
-    comb2 = compute_binomial(n_orbital+1, N_elect/2+N_spin+1)
+    comb1 = compute_binomial(n_orbital+1,N_elect//2-N_spin)
+    comb2 = compute_binomial(n_orbital+1, N_elect//2+N_spin+1)
     N_Cas = (2*N_spin+1)/(n_orbital+1) * comb1 * comb2
     return ("The output with " + str(N_elect) + " electrons and "+ str(n_orbital) +  
             " orbitals and spin " +str(N_spin) +    " is " + str(N_Cas)   )
